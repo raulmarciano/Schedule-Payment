6a36106c45a2f51a6aff55d7f12ef3b45e1ec067
raulmarciano <raul_marciano@outlook.com>
2023-08-22 22:34:22 -0300

    feat: Create base application and docker with sql server

---
+++ Scheduled.Payment.Hangfire/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Scheduled.Payment.Hangfire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Hangfire");
+        }
+    }
+}
